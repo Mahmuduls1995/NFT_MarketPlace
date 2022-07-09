49b973e518f8c2166cca634aceede4f746a3301e
mahmudul1995 <mahmuduls451@gmail.com>
2022-07-09 16:25:44 +0600

    hero section done

---
+++ src/components/ui/HeroSection.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import { Link } from "react-router-dom";
+import "./hero-section.css";
+
+import heroImg from "../../assets/images/hero.jpg";
+
+const HeroSection = () => {
+  return (
+    <section className="hero__section">
+      <Container>
+        <Row>
+          <Col lg="6" md="6">
+            <div className="hero__content"
+
+              data-aos="fade-right"
+              data-aos-offset="200"
+              data-aos-delay="50"
+              data-aos-duration="1000"
+              data-aos-mirror="true"
+              data-aos-once="false"
+
+            >
+              <h2>
+                Discover rare digital art and collect
+                <span>sell extraordinary</span> NFTs
+              </h2>
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Deleniti excepturi omnis neque adipisci sequi ullam unde in
+                minus quis quos.
+              </p>
+
+              <div className="hero__btns d-flex align-items-center gap-4">
+                <button className=" explore__btn d-flex align-items-center gap-2">
+                  <i class="ri-rocket-line"></i>{" "}
+                  <Link to="/market">Explore</Link>
+                </button>
+                <button className=" create__btn d-flex align-items-center gap-2">
+                  <i class="ri-ball-pen-line"></i>
+                  <Link to="/create">Create</Link>
+                </button>
+              </div>
+            </div>
+          </Col>
+
+          <Col lg="6" md="6">
+            <div
+              data-aos="fade-left"
+              data-aos-offset="200"
+              data-aos-delay="50"
+              data-aos-duration="1000"
+              data-aos-mirror="true"
+              data-aos-once="false"
+              className="hero__img" >
+              <img src={heroImg} alt="#" className="w-100" />
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+export default HeroSection;
