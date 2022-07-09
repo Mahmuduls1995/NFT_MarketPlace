f785cb93c88b2a8479e201e76cb7aeee962daf71
mahmudul1995 <mahmuduls451@gmail.com>
2022-07-09 16:22:06 +0600

    router set

---
+++ src/Callback.js
@@ -0,0 +1,14 @@
+//  Callback Function
+
+function pHero(webInstructor) {
+    // console.log(webInstructor);
+    webInstructor()
+
+}
+
+function webInstructor() {
+    console.log('You are selected')
+}
+
+// const webInstructor =['mahmud','Hasan']
+pHero(webInstructor)
